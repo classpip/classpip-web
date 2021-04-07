0a6340fde4af3652bc1e27c5a1b1439c0b67c560
jordisalvador7 <jordisalvador98@gmail.com>
2021-04-07 12:33:18 +0200

    intento de añadir recursos

--- src/app/pages/recursos/recursos.component.ts
+++ src/app/pages/recursos/recursos.component.ts
@@ -1,4 +1,8 @@
+import { MatTableDataSource } from '@angular/material/table';
+import { FamiliaAvatares } from './../../clases/FamiliaAvatares';
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-recursos',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecursosComponent implements OnInit {
   isCollapsed = true;
-  constructor() { }
+
+  familiasPublicas: FamiliaAvatares[]
+  propietarios: string[];
+  dataSource;
+  dataSourcePublicas;
+  profesorId: number;
+
+  constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
+    this.DameFamiliasDeAvataresPublicas();
+  }
+  DameFamiliasDeAvataresPublicas() {
+    // traigo todos los publicos excepto los del profesor
+    this.authService.DameFamiliasAvataresPublicas()
+    .subscribe ( res => {
+      if (res[0] !== undefined) {
+        this.familiasPublicas = res.filter (familia => familia.profesorId !== this.profesorId);
+        if (this.familiasPublicas.length === 0) {
+          this.familiasPublicas = undefined;
+        } else {
+          this.dataSourcePublicas = new MatTableDataSource(this.familiasPublicas);
+          this.propietarios = [];
+          // Traigo profesores para preparar los nombres de los propietarios
+          this.authService.DameProfesores()
+          .subscribe ( profesores => {
+            this.familiasPublicas.forEach (familia => {
+              const propietario = profesores.filter (p => p.id === familia.profesorId)[0];
+              this.propietarios.push (propietario.Nombre + ' ' + propietario.PrimerApellido);
+            });
+          });
+        }
+      }
+    });
   }
 
 }
