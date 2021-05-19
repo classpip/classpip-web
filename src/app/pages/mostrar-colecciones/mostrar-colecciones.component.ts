0d1d0945fc19180d09bfc8e4a871f549324b1929
Ivan Requena Garcia <ivanreq97@gmail.com>
2021-05-19 02:11:13 +0200

    grid-wrapper responsive

--- src/app/pages/mostrar-colecciones/mostrar-colecciones.component.ts
+++ src/app/pages/mostrar-colecciones/mostrar-colecciones.component.ts
@@ -57,73 +57,77 @@ export class MostrarColeccionesComponent implements OnInit {
   ngOnInit(): void {
     this.coleccion = this.sesion.DameColeccion();
     console.log("coleccion viene de sesion: ", this.coleccion)
-    this.nombreColeccion = this.coleccion.Nombre;
-    if (this.coleccion.ImagenColeccion !== undefined) {
-      this.imagenColeccion = URL.ImagenesColeccion + this.coleccion.ImagenColeccion ;
-    } else {
-      this.imagenColeccion = undefined;
-    }
-
-    this.recursos.DameCromosColeccion (this.coleccion.id)
-    .subscribe ( cromos => {
-      this.cromosColeccion = cromos;
-      console.log("cromos viene de recursos: ", this.cromosColeccion);
-      // Ahora preparo las imagenes de los cromos
-         // tslint:disable-next-line:prefer-for-of
-      for (let i = 0; i < this.cromosColeccion.length; i++) {
-
-        this.cromo = this.cromosColeccion[i];
-        this.imagenesCromosDelante[i] = URL.ImagenesCromo + this.cromo.ImagenDelante;
-        this.imagenesCromosDetras[i] = URL.ImagenesCromo + this.cromo.ImagenDetras;
-
+    if(this.coleccion != null){
+      this.nombreColeccion = this.coleccion.Nombre;
+      if (this.coleccion.ImagenColeccion !== undefined) {
+        this.imagenColeccion = URL.ImagenesColeccion + this.coleccion.ImagenColeccion ;
+      } else {
+        this.imagenColeccion = undefined;
       }
-
-    });
+  
+      this.recursos.DameCromosColeccion (this.coleccion.id)
+      .subscribe ( cromos => {
+        this.cromosColeccion = cromos;
+        console.log("cromos viene de recursos: ", this.cromosColeccion);
+        // Ahora preparo las imagenes de los cromos
+           // tslint:disable-next-line:prefer-for-of
+        for (let i = 0; i < this.cromosColeccion.length; i++) {
+  
+          this.cromo = this.cromosColeccion[i];
+          this.imagenesCromosDelante[i] = URL.ImagenesCromo + this.cromo.ImagenDelante;
+          this.imagenesCromosDetras[i] = URL.ImagenesCromo + this.cromo.ImagenDetras;
+  
+        }
+  
+      });
+    } else {
+      this.nombreColeccion = 'ERROR: No se pueden visualizar los recursos.';
+    }
   }
 
   // Busca la imagen que tiene el nombre del cromo.Imagen y lo carga en imagenCromo
   TraeImagenesCromos() {
 
     // tslint:disable-next-line:prefer-for-of
-  for (let i = 0; i < this.cromosColeccion.length; i++) {
+    for (let i = 0; i < this.cromosColeccion.length; i++) {
 
-    this.cromo = this.cromosColeccion[i];
-    this.imagenesCromosDelante[i] = URL.ImagenesCromo + this.cromo.ImagenDelante;
-    this.imagenesCromosDetras[i] = URL.ImagenesCromo + this.cromo.ImagenDetras;
+      this.cromo = this.cromosColeccion[i];
+      this.imagenesCromosDelante[i] = URL.ImagenesCromo + this.cromo.ImagenDelante;
+      this.imagenesCromosDetras[i] = URL.ImagenesCromo + this.cromo.ImagenDetras;
+
+    }
+  }
+
+  // Le pasamos la coleccion y buscamos la imagen que tiene y las imagenes de sus cromos
+  TraeImagenColeccion(coleccion: Coleccion) {
+
+    console.log('entro a buscar cromos y foto');
+    console.log(coleccion.ImagenColeccion);
+    // Si la coleccion tiene una foto (recordemos que la foto no es obligatoria)
+    if (coleccion.ImagenColeccion !== undefined) {
+
+      this.imagenColeccion = URL.ImagenesColeccion + coleccion.ImagenColeccion ;
+
+      // Sino la imagenColeccion será undefined para que no nos pinte la foto de otro equipo préviamente seleccionado
+    } else {
+      this.imagenColeccion = undefined;
+    }
+
+
+    // Una vez tenemos el logo del equipo seleccionado, buscamos sus alumnos
+    console.log('voy a mostrar los cromos de la coleccion ' + coleccion.id);
+
+    // Ordena los cromos por nombre. Asi si tengo algun cromo repetido, salen juntos
+    this.cromosColeccion.sort((a, b) => a.Nombre.localeCompare(b.Nombre));
+    this.TraeImagenesCromos();
 
   }
-}
 
-// Le pasamos la coleccion y buscamos la imagen que tiene y las imagenes de sus cromos
-TraeImagenColeccion(coleccion: Coleccion) {
-
-console.log('entro a buscar cromos y foto');
-console.log(coleccion.ImagenColeccion);
-// Si la coleccion tiene una foto (recordemos que la foto no es obligatoria)
-if (coleccion.ImagenColeccion !== undefined) {
-
-  this.imagenColeccion = URL.ImagenesColeccion + coleccion.ImagenColeccion ;
-
-  // Sino la imagenColeccion será undefined para que no nos pinte la foto de otro equipo préviamente seleccionado
-} else {
-  this.imagenColeccion = undefined;
-}
-
-
-// Una vez tenemos el logo del equipo seleccionado, buscamos sus alumnos
-console.log('voy a mostrar los cromos de la coleccion ' + coleccion.id);
-
-// Ordena los cromos por nombre. Asi si tengo algun cromo repetido, salen juntos
-this.cromosColeccion.sort((a, b) => a.Nombre.localeCompare(b.Nombre));
-this.TraeImagenesCromos();
-
-}
-
-goBack() {
-    this.router.navigateByUrl('/recursos/colecciones');
-}
-Voltear() {
-  this.voltear = !this.voltear;
-}
+  goBack() {
+      this.router.navigateByUrl('/recursos/colecciones');
+  }
+  Voltear() {
+    this.voltear = !this.voltear;
+  }
 
 }
