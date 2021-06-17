d59b656bc48a86645d833c6a9bc232eb6a3d81a8
Ivan Requena Garcia <ivanreq97@gmail.com>
2021-06-17 20:30:07 +0200

    imagenes experiencias

--- src/app/clases/Comentario.ts
+++ src/app/clases/Comentario.ts
@@ -2,17 +2,15 @@ import { Profesor } from './Profesor';
 export class Comentario {
     comentario: string;
     fecha: string;
-    likes: number;
     id: number;
     autorId: number;
     autor: Profesor;
     publicacionId: number;
     isPropietario: boolean;
 
-    constructor(comentario: string, fecha: string, likes: number, autorId: number, publicacionId: number, autor?: Profesor, id?: number) {
+    constructor(comentario: string, fecha: string, autorId: number, publicacionId: number, autor?: Profesor, id?: number) {
         this.comentario = comentario;
         this.fecha = fecha;
-        this.likes = likes;
         this.autorId = autorId;
         this.autor = autor;
         this.publicacionId = publicacionId;
