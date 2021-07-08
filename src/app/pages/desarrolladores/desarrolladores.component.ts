9de71aee504140c366f8a2be0f58539651e20a2b
Ivan Requena Garcia <ivanreq97@gmail.com>
2021-07-08 22:55:38 +0200

    desarrolladores hecho

--- src/app/pages/desarrolladores/desarrolladores.component.ts
+++ src/app/pages/desarrolladores/desarrolladores.component.ts
@@ -9,7 +9,19 @@ export class DesarrolladoresComponent implements OnInit {
   isCollapsed = true;
   constructor() { }
 
+  urlPlantilla = "https://demos.creative-tim.com/blk-design-system-angular/#/home";
+  urlDocumentacion = "https://demos.creative-tim.com/blk-design-system-angular/#/documentation/overview";
+
   ngOnInit(): void {
   }
 
+  redirectPlantilla() {
+    window.open(this.urlPlantilla);
+  }
+
+  downloadManual(){}
+
+  goDown(element){
+    element.scrollIntoView({behavior: "smooth"});
+  }
 }
